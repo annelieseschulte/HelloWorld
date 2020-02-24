2f72916a4611ec03ba6b34a2b15099a26a304b41
anneliese schulte <anneliese.schulte-hordelhoff.2022@student.lhschools.org>
2020-02-24 13:42:08 -0500

    Brought under source control

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.outprintln("Hello World");
+        System.out.print("My name is ");
+        System.out.println("My name is");
+        System.out.println("Over winter break");
+        System.out.println("I visited family");
+        System.out.println("and I also hung out with friends ");
+    }
+}
